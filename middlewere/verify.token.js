766bc4e984abd619ec69b9a5b4ae3696f835278e
hamim <hamim2114@gmail.com>
2024-09-12 15:04:47 +0600

    updated

--- middlewere/verify.token.js
+++ middlewere/verify.token.js
@@ -2,8 +2,8 @@ import jwt from "jsonwebtoken";
 import { createError } from "../utils/error.handler.js";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.poshcoder
-  // const token = req.headers.authorization
+  // const token = req.cookies.poshcoder
+  const token = req.headers.authorization
 console.log(token)
   if (!token) {
     return next(createError(401, 'You are not authenticated!'));
